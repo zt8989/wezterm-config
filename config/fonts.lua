f011ea79bd77edfe2c17eb410217278d6468ecb4
zhouteng <zhouteng@zhoutengdeMacBook-Pro.local>
2026-03-24 09:55:32 +0800

    Update font fallback and macOS default shell

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,11 +1,13 @@
 local wezterm = require("wezterm")
 local platform = require("utils.platform")
 
-local font = "JetBrainsMono NF"
 local font_size = platform().is_mac and 12 or 9
 
 return {
-  font = wezterm.font(font),
+  font = wezterm.font_with_fallback({
+    "JetBrains Mono",
+    "Symbols Nerd Font Mono",
+  }),
   font_size = font_size,
 
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
--- config/launch.lua
+++ config/launch.lua
@@ -22,7 +22,7 @@ if platform.is_win then
     },
   }
 elseif platform.is_mac then
-  options.default_prog = { "/opt/homebrew/bin/fish", "--login" }
+  options.default_prog = { "/bin/zsh", "--login" }
   options.launch_menu = {
     { label = " Bash", args = { "bash", "--login" } },
     { label = " Fish", args = { "/opt/homebrew/bin/fish", "--login" } },
